dfdd32fc64e085073e7e57d0c53ad9631aaeaa3c
Joseff Thomas <joseffthomas91@gmail.com>
2019-06-04 14:33:11 +0100

    Dynamically get categories from json

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,7 @@
         <div class="relative">
           <select v-model="category" id="category" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-2">
             <option value="">None</option>
-            <option value="1">Accounting/Finance</option>
-            <option value="4">Business/Strategic Management</option>
-            <option value="8">Education/Training/Instruction</option>
-            <option value="11">Food Services/Hospitality</option>
-            <option value="12">Human Resources</option>
+            <option v-for="category in jobCategories" :key="category.id" :value="category.id">{{ category.name }}</option>
           </select>
           <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -67,11 +63,23 @@ export default {
       evt.preventDefault();
       this.keywords = '';
       this.category = '';
-    }
+    },
   },
   created: function() {
     this.loadData()
   },
+  computed: {
+    jobCategories() {
+      return this.jobs.map(job => {
+        var category = new Object()
+        category['id'] = job.category_id;
+        category['name'] = job.category_name;
+        return category;
+      })
+      .filter((value, index, self) => self.map(cat => cat.id).indexOf(value.id) == index)
+      .sort((a, b)  => a.id - b.id)
+    }
+  }
 }
 </script>
 
